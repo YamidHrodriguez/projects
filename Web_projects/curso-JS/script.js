8433010494162242fc5269274e18fa4c7b080b1b
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:04:24 -0500

    valor null y suma de dos numeros

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -1,26 +1,7 @@
-//variable = espacio que reservamos para guardar algo en memoria
-/*
-TIPOS DE DATOS:
-String = Cadena de texto ('',"",``)
-*/
-/*DECLARAR UNA VARIABLE
-var : variable de alcance global
-let : variable de alcance de bloque
-cons : constante 
-*/
+//PRUEBAS CON PROMPT
+let numero = null; // variable que declaramos vacia.
 
-//DECLARAR UNA VARIABLE
-var string;
-let boolean;
+let numer = 5;
+let numero2 = 6;
 
-//INICIALIZAR UNA VARIABLE
-string = "Cadena de texto"
-const number = 19;
-boolean = true;
-
-alert(number);
-/*CASOS ESPECIALES:
-Undifined: la variable existe, pero está sin valor.
-Null: Valor nulo o vacio.
-NaN: Resultado de una operación númerica no valido. Como dividir por cero.
-*/
+alert(numer+numero2)
