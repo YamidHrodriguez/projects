8a18ecee6d544a96fb614ab192ed0e983d9b620a
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:24:16 -0500

    Operadores de asignación en JS

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -1,9 +1,12 @@
-//Operadores
+//OPERADORES
 
 //Aritmeticos = Aquellos que toman valores númericos (literales o variables)
+
+//De asignación = Asigna valor operando de la izquierda basado en el valor operando de la derecha.
 let numero = 10;
 
 numero +=5;
+numero *=10;
+numero /=2;
+numero -=9;
 document.write(numero)
-
-//De asignación = Asigna valor operando de la izquierda basado en el valor operando de la derecha.
