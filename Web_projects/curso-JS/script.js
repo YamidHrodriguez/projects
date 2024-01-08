ec360235f9cb7868407917bb7cf8eb798e03a301
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:08:48 -0500

    Nan

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -4,4 +4,5 @@ let numero = null; // variable que declaramos vacia.
 let numer = 5;
 let numero2 = "Pedro";
 
-alert(numer+numero2)
+alert(numer*numero2)
+//Resultado del alert : Nan = operacion invalida, con algo que no es un numero
