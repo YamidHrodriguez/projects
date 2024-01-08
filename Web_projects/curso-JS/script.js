0485573ae9d97914cba879b671ca263fd08f1540
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:17:55 -0500

    document.write()

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -1,3 +1,9 @@
-//PRUEBAS CON PROMPT
-let mensaje = prompt("Ingres un mensaje")
-alert("Tu mensaje es: "+mensaje)
+//Operadores
+
+//Aritmeticos = Aquellos que toman valores númericos (literales o variables)
+let numero = 10;
+
+numero +=5;
+document.write(numero)
+
+//De asignación = Asigna valor operando de la izquierda basado en el valor operando de la derecha.
