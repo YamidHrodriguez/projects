063620dc3eae9b578b8fff23bf0ffbb2b38374b3
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:51:48 -0500

    Operadores lógicos en JS

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -3,11 +3,20 @@
 //Aritmeticos = Aquellos que toman valores númericos (literales o variables)
 
 //De asignación = Asigna valor al operando de la izquierda basado en el valor operando de la derecha.
-let numero = 10;
+let numero = 10; //asignacion 
 
-numero +=5;
-numero *=10;
-numero /=2;
-numero -=9;
-numero%=11;
-document.write(numero)
+numero +=5; //asignación de adición
+numero *=10; //asignación de multiplicación
+numero /=2; // asignación de división
+numero -=9; // asignación de sustracción
+numero%=10; // asignación de resto
+numero**=2; // asignación de potenciación
+
+
+
+
+// Operadores lógicos
+a= true && false; // false
+b= true || false; // true
+c= !true; // false
+document.write(numero,"<br>",a,"<br>",b,"<br>",c)
