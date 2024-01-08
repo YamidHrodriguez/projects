7ec67b47f4e9f9d2121125fa790744b172e918e8
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 20:58:46 -0500

    declarar e inicializar una variable, y tipos de datos en JS

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -1,4 +1,26 @@
 //variable = espacio que reservamos para guardar algo en memoria
+/*
+TIPOS DE DATOS:
+String = Cadena de texto ('',"",``)
+*/
+/*DECLARAR UNA VARIABLE
+var : variable de alcance global
+let : variable de alcance de bloque
+cons : constante 
+*/
 
-recipiente = "barbijo";
-alert(recipiente);
+//DECLARAR UNA VARIABLE
+var string;
+let boolean;
+
+//INICIALIZAR UNA VARIABLE
+string = "Cadena de texto"
+const number = 19;
+boolean = true;
+
+alert(number);
+/*CASOS ESPECIALES:
+Undifined: la variable existe, pero está sin valor.
+Null: Valor nulo o vacio.
+NaN: Resultado de una operación númerica no valido. Como dividir por cero.
+*/
