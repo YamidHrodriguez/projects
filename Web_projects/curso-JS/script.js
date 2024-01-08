60dc9bb2a52c53994293183fa5be277fe2a5c06f
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 22:08:21 -0500

    Concatenacion en JS

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -1,25 +1,5 @@
-//OPERADORES
-
-//Aritmeticos = Aquellos que toman valores númericos (literales o variables) como sus operandos y retornan un valor númerico único.
-let numero1= 10;
-numero1++; //automento;
-numero1--; //decremento
-alert(numero1)
-//De asignación = Asigna valor al operando de la izquierda basado en el valor operando de la derecha.
-let numero = 10; //asignacion 
-
-numero +=5; //asignación de adición
-numero *=10; //asignación de multiplicación
-numero /=2; // asignación de división
-numero -=9; // asignación de sustracción
-numero%=10; // asignación de resto
-numero**=2; // asignación de potenciación
-
-
-
-
-// Operadores lógicos
-a= true && false; // false
-b= true || false; // true
-c= !true; // false
-document.write(numero,"<br>",a,"<br>",b,"<br>",c)
+//CONCATENACIÓN
+saludo = "¡Hola Yamid!";
+pregunta = " ¿Cómo estás?";
+frase = saludo + pregunta;
+document.write(frase)
