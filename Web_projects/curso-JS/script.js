3e48a2fdcba53b69b49824d399949908a5e47e46
YamidHRodriguez <horaciohabbos@gmail.com>
2024-01-07 21:26:02 -0500

    Operadores de Aritmeticos en JS

--- Web_projects/curso-JS/script.js
+++ Web_projects/curso-JS/script.js
@@ -2,11 +2,12 @@
 
 //Aritmeticos = Aquellos que toman valores númericos (literales o variables)
 
-//De asignación = Asigna valor operando de la izquierda basado en el valor operando de la derecha.
+//De asignación = Asigna valor al operando de la izquierda basado en el valor operando de la derecha.
 let numero = 10;
 
 numero +=5;
 numero *=10;
 numero /=2;
 numero -=9;
+numero%=11;
 document.write(numero)
